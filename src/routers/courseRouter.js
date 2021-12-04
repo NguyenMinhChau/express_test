8c1d87af486ff2d01fd0139a48bc95f933b0d9d6
Nguyễn Minh Châu <81848005+NguyenMinhChau@users.noreply.github.com>
2021-12-04 19:36:15 +0700

    ExpressJS

--- src/routers/courseRouter.js
+++ src/routers/courseRouter.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const courseController = require('../controllers/CourseController');
-
 router.get('/addCourse', courseController.AddCourse);
 router.post('/create', courseController.CreateCourse);
 router.get('/:slug', courseController.CourseDetail);
-
 module.exports = router;
--- src/routers/siteRouter.js
+++ src/routers/siteRouter.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
 const SiteController = require('../controllers/SiteController');
-
 router.get('/', SiteController.homePage);
-
 module.exports = router;
